feaa669bd24f48cc09db10236b5f17a5e117e680
Oreva <khemical51@gmail.com>
2023-09-11 22:24:42 +0100

    add footer, make stay at the bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,84 +1,119 @@
-import { Container, Grid, Typography, Link } from '@mui/material';
+import { Container, 
+  Grid, 
+  Typography, 
+  Link, 
+  Stack, 
+  List, 
+  ListItem,
+  Box,
+ } from '@mui/material';
+ import Divider from '@mui/joy/Divider';
+ import InstagramIcon from '@mui/icons-material/Instagram';
+ import TwitterIcon from '@mui/icons-material/Twitter';
+ import FacebookIcon from '@mui/icons-material/Facebook';
+ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export const Footer = () => {
+   // Get the current date
+   const currentDate = new Date();
+
+   // Format the date as "YYYY-MM-DD"
+   const formattedDate = `${currentDate.getFullYear()}`
+    
   return (
-    <footer style={{ 
-      backgroundColor: '#333',
-      bottom: '0',
-      width: '100%',
-      color: '#fff', padding: '2rem 0'}}>
+    <footer style={{ backgroundColor: '#333', color: '#fff', 
+      padding: '2rem 0', width: '100%', fontSize: '18px',
+      textAlign: 'center', position: 'absolute', 
+      right: '0', left: '0', bottom: '0'}}>
       <Container maxWidth="lg">
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6} md={3}>
-            <Typography variant="h6" fontWeight="bold" gutterBottom>
+        <Grid container spacing={1} textAlign='left'>
+          <Grid item xs={4} sm={3}>
+            <Typography variant="body1" pb={0} gutterBottom>
               Your Store
             </Typography>
             <Typography variant="body2" paragraph>
               The best place to shop online.
             </Typography>
           </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Typography variant="h6" gutterBottom>
+          <Grid item xs={4} sm={3}>
+            <Typography variant="body1" pb={0} gutterBottom>
               Quick Links
             </Typography>
-            <ul>
-              <li>
-                <Link href="/products" color="inherit">
+            <List sx={{p: '0'}}>
+              <ListItem>
+                <Link href="/products" color="inherit" variant="body2">
                   Products
                 </Link>
-              </li>
-              <li>
-                <Link href="/about" color="inherit">
+              </ListItem>
+              <ListItem>
+                <Link href="/about" color="inherit" variant="body2">
                   About Us
                 </Link>
-              </li>
-              <li>
-                <Link href="/contact" color="inherit">
+              </ListItem>
+              <ListItem>
+                <Link href="/contact" color="inherit" variant="body2">
                   Contact Us
                 </Link>
-              </li>
-            </ul>
+              </ListItem>
+            </List>
           </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Typography variant="h6" gutterBottom>
+          <Grid item xs={4} sm={3}>
+            <Typography variant="body1" pb={0} gutterBottom>
               Customer Service
             </Typography>
-            <ul>
-              <li>
-                <Link href="/faq" color="inherit">
+            <List sx={{p: '0'}}>
+              <ListItem>
+                <Link href="/faq" color="inherit" variant="body2">
                   FAQ
                 </Link>
-              </li>
-              <li>
-                <Link href="/returns" color="inherit">
+              </ListItem>
+              <ListItem>
+                <Link href="/returns" color="inherit" variant="body2">
                   Returns & Exchanges
                 </Link>
-              </li>
-              <li>
-                <Link href="/shipping" color="inherit">
+              </ListItem>
+              <ListItem>
+                <Link href="/shipping" color="inherit" variant="body2">
                   Shipping Information
                 </Link>
-              </li>
-            </ul>
+              </ListItem>
+            </List>
           </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Typography variant="h6" gutterBottom>
+          <Grid item xs={4} sm={3}>
+            <Typography variant="body1" pb={0} gutterBottom>
               Stay Connected
             </Typography>
-            <ul>
-              <li>
-                <Link href="/newsletter" color="inherit">
+            <List sx={{p: '0'}}>
+              <ListItem >
+                <Link href="/newsletter" color="inherit" variant="body2">
                   Newsletter
                 </Link>
-              </li>
-              <li>
-                <Link href="/social" color="inherit">
+              </ListItem>
+              <ListItem>
+                <Link href="/social" color="inherit" variant="body2">
                   Social Media
                 </Link>
-              </li>
-            </ul>
+              </ListItem>
+            </List>
           </Grid>
         </Grid>
+        <Stack display='flex' textAlign='left' justifyContent='center' width={152} py={2}>
+          <Typography>Join Us</Typography>
+          <Box>
+            <FacebookIcon sx={{p: '5px'}}/>
+            <InstagramIcon sx={{p: '5px'}}/>
+            <TwitterIcon sx={{p: '5px'}}/>
+            <LinkedInIcon sx={{p: '5px'}}/>
+          </Box>
+        </Stack>
+        <Divider sx={{my: '4px'}}/>
+        <Stack >
+            {/* Display the current date */}
+          <Typography variant="body2" sx={{ mt: '1rem' }}>
+            &copy; {formattedDate} <Link href='https://github.com/OrevaTJ' color='inherit'>
+              OrevaTJ</Link>. All rights reserved.
+          </Typography>
+        </Stack>
       </Container>
     </footer>
   );
