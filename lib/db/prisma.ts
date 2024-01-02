fdbff29e7afca216af056d270a0994e96879be97
Oreva <khemical51@gmail.com>
2024-01-02 21:08:58 +0100

    update prisma client

--- lib/db/prisma.ts
+++ lib/db/prisma.ts
@@ -10,6 +10,17 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleton | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+const prismaBase = globalForPrisma.prisma ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = prismaBase.$extends({
+  query: {
+    cart: {
+      async update({ args, query }) {
+        args.data = { ...args.data, updatedAt: new Date() };
+        return query(args);
+      },
+    },
+  },
+});
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prismaBase;
